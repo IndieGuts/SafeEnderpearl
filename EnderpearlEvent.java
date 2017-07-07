2da511c5e34915a57ae875d99ff5f1cd2751002c
IndieGuts <IndieGuts@yahoo.com>
2017-07-07 01:54:21 -0700

    Added EventPriority
    Added Event Priority, no version update..

--- EnderpearlEvent.java
+++ EnderpearlEvent.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
@@ -20,11 +21,14 @@ public class EnderpearlEvent implements Listener {
 		this.core = core;
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void teleportEvent(PlayerTeleportEvent event) {
 		if (event.getCause() == TeleportCause.ENDER_PEARL) {     
 			Player player = (Player) event.getPlayer();
 			Location locto = event.getTo();
+			if (player.hasPermission("se.bypass")){
+				return;
+			} else {
 			if (locto.add(0, 1, 0).getBlock().getType().isSolid()) {
 				player.playSound(player.getLocation(), Sound.ENTITY_IRONGOLEM_HURT, 10, 10);
 				event.setCancelled(true);
@@ -34,6 +38,8 @@ public class EnderpearlEvent implements Listener {
 				} else {
 					player.sendMessage(ChatColor.translateAlternateColorCodes('&', "" + core.getConfig().getString("messages.deny-enderpearl")));
 				}
+				
+			}
 			}
 		}
 	}
